438afa061625342b296b86cdc4abe963152988a7
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-11-21 16:33:06 +0900

    add: types

--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -1,5 +1,6 @@
 export type FileType = 'png' | 'jpeg';
 export type Theme = 'light' | 'dark';
+export type Background = 'none' | 'blog';
 
 export interface ParsedRequest {
     fileType: FileType;
@@ -7,7 +8,6 @@ export interface ParsedRequest {
     theme: Theme;
     md: boolean;
     fontSize: string;
-    images: string[];
-    widths: string[];
-    heights: string[];
+    background: string;
+    blog_author?: string;
 }
